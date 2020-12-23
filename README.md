103e790c8493c77d53f0f22d4fdc44ce538c293b
charlie <charliecondorcet@gmail.com>
2020-12-23 03:09:54 -0300

    docs(main): undate documentation

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # ParallelFibonacci
-Fibonacci code with OpenMP in C
+###Fibonacci code with OpenMP in C
+
+The code is made with a special library called GMP, which proves 
+telescopic numbers and functions to operate them.
+
+with commands to install the library in ubuntu
+                            
+    sudo apt-get install libgmp3-dev
+    
+The principal file to ejecute the code is **main.c**
+
--- main.c
+++ main.c
@@ -10,17 +10,19 @@
 
 void factorizar(mpz_t fibNum) {
 
+    // variable declaration and initialization
     mpz_t divisor;
     mpz_init(divisor);
     mpz_set_ui(divisor, 2);
 
-    // if (fibNum <= 5) {
+    // if (fibNum <= 5) show the minimum factorial
     if (mpz_cmp_d(fibNum, 5) <= 0) {
         gmp_printf("%Zd ", fibNum);
 
     } else {
         while (mpz_get_ui(fibNum) != 1) {
 
+            // print te validated value to factorization
             if (mpz_get_ui(fibNum) % mpz_get_ui(divisor) == 0) {
                 gmp_printf("%Zd ", divisor);
                 mpz_cdiv_q(fibNum, fibNum, divisor);
@@ -29,12 +31,14 @@ void factorizar(mpz_t fibNum) {
                     printf("x ");
 
             } else {
+                // divide for the factor founded
                 mpz_add_ui(divisor, divisor, 1);
             }
         }
 
     }
 
+    // clear the memory usage
     mpz_clear(divisor);
 }
 
@@ -47,14 +51,17 @@ int main(int argc, char **argv) {
 
     printf("---------- Fibonacci code print and factorization ----------\n");
 
+    // fibonacci variables
     mpz_t frstFib;
     mpz_t scndFib;
     mpz_t fibonacci;
 
+    // initialization
     mpz_init_set_str(frstFib, "0", 10);
     mpz_init_set_str(scndFib, "1", 10);
     mpz_init_set_str(fibonacci, "1", 10);
 
+    // catch the data received for command line
     char *a = argv[1];
     int n = atoi(a);
 
@@ -62,22 +69,25 @@ int main(int argc, char **argv) {
 
     for (int i = 1; i <= n; ++i) {
 
+        // print all fibonacci series founded
         gmp_printf("%d : %Zd = ", i, fibonacci);
 
+        // print the factorization to fibonacci series
         factorizar(fibonacci);
         printf("\n");
 
+        // calculate the next series
         mpz_add(fibonacci, frstFib, scndFib);
         mpz_set(frstFib, scndFib);
         mpz_set(scndFib, fibonacci);
 
     }
 
+    // clear variables GMP
     mpz_clear(frstFib);
     mpz_clear(scndFib);
     mpz_clear(fibonacci);
 
     printf("\nFinalization ..\n");
-
     return 0;
 }
